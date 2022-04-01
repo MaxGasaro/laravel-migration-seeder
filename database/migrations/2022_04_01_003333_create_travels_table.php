510d93ed34346fab09cba20ad14723ab06a4626d
Massimiliano Gasaro <massimilianogasaro95@gmail.com>
2022-04-01 03:14:32 +0200

    secondo commit

---
+++ database/migrations/2022_04_01_003333_create_travels_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('city', 20);
+            $table->string('country', 20);
+            $table->decimal('costo',7,2);
+            $table->text('description');
+            $table->date('partenza');
+            $table->date('ritorno');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
